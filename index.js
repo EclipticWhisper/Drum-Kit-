246a5f97e32d39c88f7e1c800fafe7e9ee6bf750
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-07-25 20:05:51 +0500

    Added Interactivity with Keyboard Events

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+// 1. The unified logic function (handles both clicks and keys)
+function makeBreakSound(key) {
+    // Convert to lowercase so Caps Lock doesn't break the instrument
+    switch (key.toLowerCase()) {
+        case "w":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "a":
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+        case "s":
+            new Audio("sounds/tom-3.mp3").play();
+            break;
+        case "d":
+            new Audio("sounds/tom-4.mp3").play();
+            break;
+        case "j":
+            new Audio("sounds/snare.mp3").play();
+            break;
+        case "k":
+            new Audio("sounds/crash.mp3").play();
+            break;
+        case "l":
+            new Audio("sounds/kick-bass.mp3").play();
+            break;
+        default:
+            console.log(`No sound assigned to: ${key}`);
+    }
+}
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        var buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
-            case "w":
-                var tom1 = new Audio("sounds/tom-1.mp3");
-                tom1.play();
-                break;
-            case "a":
-                var tom2 = new Audio("sounds/tom-2.mp3");
-                tom2.play();
-                break;
-            case "s":
-                var tom3 = new Audio("sounds/tom-3.mp3");
-                tom3.play();
-                break;
-            case "d":
-                var tom4 = new Audio("sounds/tom-4.mp3");
-                tom4.play();
-                break;
-            case "j":
-                var snare = new Audio("sounds/snare.mp3");
-                snare.play();
-                break;
-            case "k":
-                var crash = new Audio("sounds/crash.mp3");
-                crash.play();
-                break;
-            case "l":
-                var kick = new Audio("sounds/kick-bass.mp3");
-                kick.play();
-                break;
-            default: console.log(buttonInnerHTML);
-        }
+// 2. Click Event Listener
+document.querySelectorAll(".drum").forEach(button => {
+    button.addEventListener("click", function () {
+        makeBreakSound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
     });
+});
+
+// 3. Keyboard Event Listener
+document.addEventListener("keydown", function (event) {
+    makeBreakSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
